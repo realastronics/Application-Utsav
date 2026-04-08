85a42994a0e7b589a3816a25094e8b7f3047767c
mitali_salhotra <mitalisalhotra@gmail.com>
2026-04-08 18:35:07 +0530

    Initial UI and project setup

---
+++ app/src/main/java/com/utsav/app/MainActivity.java
@@ -0,0 +1,37 @@
+package com.utsav.app;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.utsav.app.fragments.HomeFragment;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        BottomNavigationView bottomNav =
+                findViewById(R.id.bottomNav);
+
+        loadFragment(new HomeFragment());
+
+        bottomNav.setOnItemSelectedListener(item -> {
+            int id = item.getItemId();
+            if (id == R.id.nav_home) {
+                loadFragment(new HomeFragment());
+            }
+            //   other tabs added later
+            return true;
+        });
+    }
+
+    private void loadFragment(Fragment fragment) {
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.fragmentContainer, fragment)
+                .commit();
+    }
+}
